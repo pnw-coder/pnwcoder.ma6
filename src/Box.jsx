f188db38d01652182d435dceb0dd6df366c95028
burnalex <burns.alex@northeasterne.du>
2024-03-13 12:38:26 -0700

    "Fixed some bugs and added comments"

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react'
 import './Box.css'
 
+/*
+Box component represents a clickable box with a function to change the box's background 
+color, uses handleClick prop from parent componen to update its current state.
+*/
 function Box({ handleClick }) {
+  // Initialize state to track whether a box is on or off
   const [isOn, setIsOn] = useState(false);
 
+  // Function to handle the box click event turns it on or off
   const handleBoxClick = () => {
+    // Toggles the state of the box
     const newState = !isOn;
     setIsOn(newState);
+    // Call the parent component's click handler and passes the new state
     handleClick(newState);
   }
- 
+  
+  // Initializes a variable that determines the className based on the box's state
   const boxClassName = `box ${isOn ? 'selected' : ''}`;
 
+  // Renders the box as a button with appropriate className based on its state
   return (
     <button className={boxClassName} onClick={handleBoxClick}></button>
   );
--- src/Grid.jsx
+++ src/Grid.jsx
@@ -2,14 +2,21 @@ import { useState } from 'react'
 import Box from './Box'
 import './Grid.css'
 
-
+/*
+Grid component creates a 2x2 grid of Box components, using the state of the box to update the counter
+function, if a box is on the counter is incremented if it is off it is decremeneted.
+*/
 function Grid() {
+  // Initializes the state to track the count of selected boxes
   const [count, setCount] = useState(0)
 
+  // Function to handle the click events on boxes and increment or decrement the count
   const handleClick = (isOn) => {
+    // Update the count based on whether a box's state is set to on or off
     setCount((prevCount) => (isOn ? prevCount + 1: prevCount - 1));
   }
- 
+  
+  // Renders the Grid component with counter as the header
   return (
     <div>
         <div className='count-header'>
